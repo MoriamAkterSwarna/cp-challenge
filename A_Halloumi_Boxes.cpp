61f63c16599591cfda6482c0ddb583874fe379b5
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-08-19 23:41:06 +0600

    first commit

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define tst   \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void firebird_solve()
+{
+
+    long long n , k;
+    cin >> n >> k;
+
+    vector<long long> arr(n);
+
+
+    for(long long i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+
+    vector<long long> arr2 = arr;
+    sort(arr2.begin(), arr2.end());
+
+    if(arr == arr2 || k > 1)
+    {
+        cout << "YES" << nl;
+    }
+    else
+    {
+        cout << "NO" << nl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    tst
+    {
+        firebird_solve();
+    }
+
+    return 0;
+}
